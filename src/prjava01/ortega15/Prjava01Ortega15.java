617fc4195c58823f45a00012d891786bc41b2b71
u1w7 <u1w7@192.168.1.212>
2015-04-29 18:16:52 +0100

    Versió 0.2 del projecte prjava01-ortega

--- src/prjava01/ortega15/Prjava01Ortega15.java
+++ src/prjava01/ortega15/Prjava01Ortega15.java
@@ -20,7 +20,8 @@ public class Prjava01Ortega15 {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) { 
             bw.write("<html>");bw.newLine(); bw.write(" <head>");
             bw.newLine(); bw.write(" <title>");bw.newLine(); 
-            bw.write(" Nova p&agrave;gina web de Jesus Ortega");bw.newLine(); 
+            bw.write(" Nova p&agrave;gina web de Jesus Ortega");bw.newLine();
+            bw.write(" DAW2 UF4 Pr&agrave;ctica 2 ");bw.newLine(); 
             bw.write(" </title>");bw.newLine(); bw.write(" </head>");
             bw.newLine(); bw.write(" <body>");bw.newLine();
             bw.write(" Nova p&agrave;gina web de Jesus Ortega");bw.newLine();
